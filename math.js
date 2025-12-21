b53cf7da8d612370be1502fab0bd8cefdc2d51f0
Nicholas Brandt <git@nicholasbrandt.de>
2025-12-21 15:03:22 +0100

    include math / cleanup

--- math.js
+++ math.js
@@ -1,4 +1,4 @@
-import katex from 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.mjs';
+// import katex from 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.mjs';
 import renderMathInElement from 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.mjs';
 
 renderMathInElement(document.body);
